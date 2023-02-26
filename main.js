9dc494ae2a583ec0a4ee7b098faef9fb1134fc4c
Azizbek <xasanboyev283@gmail.com>
2023-02-26 21:51:15 +0300

    aded slide

--- main.js
+++ main.js
@@ -34,3 +34,41 @@ secMenu.addEventListener('click', (event) => {
   console.log(event.target.children[1].classList);
   event.target.nextElementSibling.classList.toggle('show')
 })
+
+let slide1 = document.querySelector('.slide1')
+let slide2 = document.querySelector('.slide2')
+let slide3 = document.querySelector('.slide3')
+let slideAction = document.querySelector('.slide__action');
+let count = 0;
+setInterval( () => {
+
+  if( count == 0 ) {
+    slide2.style.display = 'none';
+    slide3.style.display = 'none';
+    slide1.style.display = 'block';
+    slideAction.children[0].classList.add('slide__active')
+    slideAction.children[1].classList.remove('slide__active')
+    slideAction.children[2].classList.remove('slide__active')
+    count += 1;
+    console.log(1);
+  }else if(count == 1 ) {
+    slide1.style.display = 'none';
+    slide2.style.display = 'block';
+    slideAction.children[1].classList.add('slide__active')
+    slideAction.children[0].classList.remove('slide__active')
+    slideAction.children[2].classList.remove('slide__active')
+    count += 2;
+    console.log(2);
+  }  else {
+    slide1.style.display = 'none';
+    slide2.style.display = 'none';
+    slide3.style.display = 'block';
+    slideAction.children[2].classList.add('slide__active')
+    slideAction.children[1].classList.remove('slide__active')
+    slideAction.children[0].classList.remove('slide__active')
+    count = 0;
+  }
+  console.log('function');
+}, 8000)
+
+
